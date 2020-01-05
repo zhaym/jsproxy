014c86bad942df26d1844a086199a8a02f043508
zhaym <minglezhangym@gmail.com>
2020-01-06 01:06:06 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -37,9 +37,6 @@ jsproxy_config({
       label: '',
       hidden: true,
       lines: {
-        // 收费版（高权重）
-        'agent.go4u.worders.dev': 4,
-
         // 免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
         'b.007.workers.dev': 1,
